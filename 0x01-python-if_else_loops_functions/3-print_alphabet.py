f45cd7c3701ef790953bd1d62827ae7e18b59ccc
Ebunoluwa oke <ebunoke18@gmail.com>
2022-12-25 16:09:20 -0800

    Prints the alphabet without letters e and q

---
+++ 0x01-python-if_else_loops_functions/3-print_alphabet.py
@@ -0,0 +1,7 @@
+#!/usr/bin/python3
+import string
+alphabet = string.ascii_lowercase
+for letter in alphabet:
+    if (letter == "q") or (letter == "e"):
+        continue
+    print(letter, end="")
