695e3d605ffad49db34df33a24aaecaaec045ca2
Ebunoluwa oke <ebunoke18@gmail.com>
2023-01-14 02:59:29 -0800

    Last digit

---
+++ 0x01-python-if_else_loops_functions/1-last_digit.py
@@ -0,0 +1,16 @@
+#!/usr/bin/python3
+import random
+number = random.randint(-10000, 10000)
+
+print(f"Last digit of {number}", end="")
+number = list(str(number))
+i = number[len(number) - 1]
+i = int(i)
+if i > 5:
+    text = "and is greater than 5"
+elif i == 0:
+    text = "and is 0"
+elif (i < 6) and (i != 0):
+    text = "and is less than 6 and not 0"
+
+print(f" is {i} {text}")
