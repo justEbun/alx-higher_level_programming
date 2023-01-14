1c250ab0f567dc810f6ec7d882df62ffa6ca05cf
Ebunoluwa oke <ebunoke18@gmail.com>
2023-01-14 03:05:39 -0800

    Last digit

--- 0x01-python-if_else_loops_functions/1-last_digit.py
+++ 0x01-python-if_else_loops_functions/1-last_digit.py
@@ -6,6 +6,9 @@ print(f"Last digit of {number}", end="")
 number = list(str(number))
 i = number[len(number) - 1]
 i = int(i)
+if '-' in number:
+    i = -i
+
 if i > 5:
     text = "and is greater than 5"
 elif i == 0:
