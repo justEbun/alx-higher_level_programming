e8090a9280f48b9d439367356b9f07962d7552cf
Ebunoluwa oke <ebunoke18@gmail.com>
2023-01-15 08:41:10 -0800

    Adds two numbers using an imported function

--- 0x02-python-import_modules/0-add.py
+++ 0x02-python-import_modules/0-add.py
@@ -1,7 +1,7 @@
 #!/usr/bin/python3
-if "__name __" == "__main__":
+if __name __ == "__main__":
     from add_0 import add
 
-a = 1
-b = 2
-print("{} + {} = {}".format(a, b, add(1, 2))
+    a = 1
+    b = 2
+    print("{} + {} = {}".format(a, b, add(1, 2)))
