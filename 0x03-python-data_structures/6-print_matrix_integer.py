56001477fbcd45884a0552b4b7ce487eaa979d70
Ebunoluwa oke <ebunoke18@gmail.com>
2023-01-21 11:45:02 -0800

    Testing

--- 0x03-python-data_structures/6-print_matrix_integer.py
+++ 0x03-python-data_structures/6-print_matrix_integer.py
@@ -2,5 +2,7 @@
 def print_matrix_integer(matrix=[[]]):
     for each in matrix:
         for num in each:
-            print("{:d} ".format(num), end="")
+            print("{:d}".format(num), end="")
+            if num != (len(each) - 1):
+                print(" ", end="")
         print("")
