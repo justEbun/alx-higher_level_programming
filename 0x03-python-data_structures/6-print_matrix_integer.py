48da5a3885ea8bf8c5b10097c30eb249b1fceedd
Ebunoluwa oke <ebunoke18@gmail.com>
2023-01-21 03:30:26 -0800

    Prints a matrix of integer

--- 0x03-python-data_structures/6-print_matrix_integer.py
+++ 0x03-python-data_structures/6-print_matrix_integer.py
@@ -2,5 +2,5 @@
 def print_matrix_integer(matrix=[[]]):
     for each in matrix:
         for num in each:
-            print("{:d} ".format(num))
+            print("{:d} ".format(num), end="")
         print("")
